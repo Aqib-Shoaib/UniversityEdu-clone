d7de49d062a7f0142eef9f5aa1b53ebe5ac55878
Aqib-Shoaib <118466147+Aqib-Shoaib@users.noreply.github.com>
2024-07-10 20:50:56 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>hello univeristy edu...</h1>
+      <h1
+        style={{
+          fontFamily: "Times New Roman",
+        }}
+      >
+        hello univeristy edu...
+      </h1>
+    </div>
+  );
+}
+export default App;
